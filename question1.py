dc546be884e56ae238e94bd4d34783fdcf15aecb
Francesca49 <011001@britishschool.be>
2026-01-15 12:46:02 +0100

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,8 +1,6 @@
 # Write code that counts the number of anyLetter in anyText
 def countLetters(anyText:str,anyLetter:str)->int:
   counter = 0
-  anyText = input("what is your text?")
-  anyLetter = input("what letter are you looking for?")
   for anyLetter in anyText: 
     if anyLetter == anytext:
       counter + 1
